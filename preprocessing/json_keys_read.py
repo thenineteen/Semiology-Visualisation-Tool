60a75f89ac9753d060b5c70154752301336fb32a
The Nineteen <thenineteen@gmail.com>
2019-02-22 14:22:04 +0000

    json_keys_read finds MRNs and labels outcomes.

---
+++ preprocessing/json_keys_read.py
@@ -0,0 +1,51 @@
+def find_MRN_label_outcomes(list_MRNs_surgery_ILAE_1_all_follow_up_years,
+             list_MRNs_surgery_not_gold_standard_outcome,
+             json_file = "L:\\word_docs\\word_keys2.json",
+             save_new_json_dict = "L:\\word_docs\\word_keys_outcomes.json"):
+
+    """
+    Opens the word_keys.json file containing a dictionary of sensitive data.
+    Searches for specified MRNs, adds mdt/surgery outcome.
+
+    >list_MRNs: list of pre-specified MRNs.
+
+    use like this:
+    Gold_MRNs = ['QSD123456', '12345678']
+    had_surgery = ['hosp no', 'MRN']
+    sensitive_data_outcomes = find_MRN_label_outcomes(Gold_MRNs, had_surgery)
+    """
+    # open json key
+    with open(json_file) as f:
+        sensitive_data = json.load(f)
+
+# for testing and creating list of MRNs:
+    # had_surgery = []
+    # for pseudo_anon_key in sensitive_data.keys():
+    #     if int(pseudo_anon_key) > 5 and int(pseudo_anon_key)<15:
+    #         surgery.append(sensitive_data[pseudo_anon_key][MRN])
+
+    # loop through all keys    
+    for pseudo_anon_key in sensitive_data.keys():
+        
+        # label gold standard outcomes
+        if sensitive_data[pseudo_anon_key][MRN] in list_MRNs_surgery_ILAE_1_all_follow_up_years:
+            sensitive_data[pseudo_anon_key]['MDT_Surgery_Outcome'] = "Gold ILAE 1"
+
+        # label surgical resection without gold standard outcomes
+        elif sensitive_data[pseudo_anon_key][MRN] in list_MRNs_surgery_not_gold_standard_outcome:
+            sensitive_data[pseudo_anon_key]['MDT_Surgery_Outcome'] = "Resection"
+        
+        else:
+            sensitive_data[pseudo_anon_key]['MDT_Surgery_Outcome'] = "No surgery"
+
+    with open(save_new_json_dict, 'w') as file:
+
+                file.seek(0)  # rewind
+
+                json.dump(sensitive_data, file)
+                #file.write(json.dumps(pseudo_anon_dict))
+
+                file.truncate()
+
+    return sensitive_data_outcomes
+
