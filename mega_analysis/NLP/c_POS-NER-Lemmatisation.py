29aad3a2578ab47ccc22728ea9af4fdf87c9f8fd
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2020-01-17 14:53:04 +0000

    Remove unused files

--- mega_analysis/NLP/c_POS-NER-Lemmatisation.py
+++
@@ -1,5 +0,0 @@
-import nltk
-from b_1_filter_and_tokenise import *
-
-# NLTK doesn't seem to do this well - all are NNP. Try other NLP toolkits for this Pos tagging and NER
-# prior to Lemmatisation. 
