3282ff6b61f4ba1b599458c0f2d2a7d01872293b
Fernando Perez-Garcia <fepegar@gmail.com>
2020-03-27 14:04:08 +0000

    Move requirements to separate file for Slicer

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt") as fh:
+    requirements = fh.readlines()
+
 setup(
     name="mega_analysis",
     version="0.1.0",
@@ -16,19 +19,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=['*tests']),
-    install_requires=[
-        'click',
-        'docx',
-        'matplotlib',
-        'matplotlib-venn',
-        'pandas',
-        'PyPDF2',
-        'PyYaml',
-        'scikit-learn',
-        'seaborn',
-        'uuid',
-        'xlrd',
-    ],
+    install_requires=requirements,
     entry_points={
         'console_scripts': [
             'Process_Epilepsy_Docx = mega_analysis.preprocessing.command:main_docx_preprocess',
