45caa4ecdfc71de610a31c96df9f724c0a772985
Fernando Perez-Garcia <fepegar@gmail.com>
2020-03-27 17:15:42 +0000

    Fix functions indentation

--- mega_analysis/crosstab/mega_analysis/exclusions.py
+++ mega_analysis/crosstab/mega_analysis/exclusions.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
-def exclusions(df, 
+def exclusions(df,
                 POST_ictals=True,
                 PET_hypermetabolism=True,
                 SPECT_PET=False,
@@ -16,7 +16,7 @@ def exclusions(df,
     SPECT_PET also excludes fMRI DTI when no structural MRI lesion.
 
     """
-    
+
     post_op = 'Post-op Sz Freedom (Engel Ia, Ib; ILAE 1, 2)'
     concordant = 'Concordant Neurophys & Imaging (MRI, PET, SPECT)'
     # sEEG_ES = 'sEEG and/or ES'
@@ -25,7 +25,7 @@ def exclusions(df,
 
     if POST_ictals:
         post_ictals = ['post-ictal', 'postictal', 'post ictal', 'post_ictal']
-        post_ictal_inspection = QUERY_SEMIOLOGY(df, semiology_term=post_ictals, 
+        post_ictal_inspection = QUERY_SEMIOLOGY(df, semiology_term=post_ictals,
                                                 ignore_case=True, use_semiology_dictionary=False)
         df.drop(labels = post_ictal_inspection.index, axis='index', inplace=True, errors='ignore')
         print('Excluded post-ictal semiology in the query')
@@ -34,14 +34,14 @@ def exclusions(df,
         col1 = concordant
         pet = ['PET']
 
-        pet_inspection = QUERY_SEMIOLOGY(df, semiology_term=pet, 
+        pet_inspection = QUERY_SEMIOLOGY(df, semiology_term=pet,
                                                 ignore_case=True, use_semiology_dictionary=False,
                                                 col1=col1, col2=col1)
 
         hyper = ['Hyper']
-        hyper_inspection = QUERY_SEMIOLOGY(df, semiology_term=hyper, 
+        hyper_inspection = QUERY_SEMIOLOGY(df, semiology_term=hyper,
                                                 ignore_case=True, use_semiology_dictionary=False,
-                                                col1=col1, col2=col1)  
+                                                col1=col1, col2=col1)
 
         # ans is df of the intersection of the above two, keep the index otherwise it is reset
         ans = hyper_inspection.reset_index().merge(pet_inspection, how="inner").set_index('index')
@@ -59,8 +59,8 @@ def exclusions(df,
 
         # # find the indices where concordance was PET hypermetabolism but other criteria were notnull
         # mixed_ground_truth_index = [item for item in list(ans.index) if item not in list(ans3.index)]
-        # df.loc[mixed_ground_truth_index][concordant] =  np.nan # doesn't drop, as meet other ground truth criteria. Just turn concordance to null. 
-        
+        # df.loc[mixed_ground_truth_index][concordant] =  np.nan # doesn't drop, as meet other ground truth criteria. Just turn concordance to null.
+
         print('Excluded cases where PET hypermetabolism was the only grund truth criteria from the query, converted rest to nulls')
 
 
@@ -69,7 +69,7 @@ def exclusions(df,
         col2 = col1
         spect_pet = ['SPECT', 'PET', 'FDG-PET', 'SPECT scan', 'PET scan', 'ictal SPECT', 'interictal PET', 'inter-ictal PET',
                      'PET+ictal SPECT', 'Surgical finding, PET hypometabolism', 'fMRI+DTI', 'PET (interictal hypometabolism)']
-        spect_pet_inspection = QUERY_SEMIOLOGY(df, semiology_term=spect_pet, 
+        spect_pet_inspection = QUERY_SEMIOLOGY(df, semiology_term=spect_pet,
                                                 ignore_case=True, use_semiology_dictionary=False,
                                                 col1=col1, col2=col2)
 
@@ -78,44 +78,37 @@ def exclusions(df,
 
         # change to nans:
         # mixed_ground_truth_index = [item for item in list(spect_pet_inspection.index) if item not in list(ans2.index)]
-        df.loc[list(ans.index), concordant] =  np.nan # doesn't drop, as meet other ground truth criteria. 
+        df.loc[list(ans.index), concordant] =  np.nan # doesn't drop, as meet other ground truth criteria.
 
         #drops pure spect/pet cases
         ans2 = ans.reset_index().merge(df.loc[df[sEEG_ES].isnull()], how='inner').set_index('index')
-        df.drop(labels = list(ans2.index), axis='index', inplace=True, errors='ignore')  
+        df.drop(labels = list(ans2.index), axis='index', inplace=True, errors='ignore')
         print('Excluded cases where concordance involved SPECT or PET without MRI or other ground truths,')
         print('converted rest to nulls')
-
-
 
     df = df.dropna(axis='columns', how='all')
 
     # concordance after dropping columns, otherwise would drop this column
     if CONCORDANCE:
-        df.loc[:, concordant] = np.nan 
+        df.loc[:, concordant] = np.nan
         print('\nEntirely replaced concordant column with nans')
         # now need to recheck and drop any rows which have no ground truth:
         df.dropna(subset=[post_op, concordant, sEEG_ES], thresh=1, axis=0, inplace=True)
-
-        
-
     return df
 
 
-    def exclude_ET(df):
-        """
-        exclude epilepsy topology cases on the fly as the data grows, rather than using the pickled resources
-        """
-        
-        df_exclusions_ET = df.dropna(subset=['Epilepsy Topology (ET)'], axis=0, inplace=False)
-        return df_exclusions_ET
+def exclude_ET(df):
+    """
+    exclude epilepsy topology cases on the fly as the data grows, rather than using the pickled resources
+    """
+    df_exclusions_ET = df.dropna(subset=['Epilepsy Topology (ET)'], axis=0, inplace=False)
+    return df_exclusions_ET
 
 
-    def exclude_sEEG_ES(df):
-        """
-        exclude stereoEEG and electrical stimulation cases on the fly and
-        as the data grows, rather than using pickled resources
-        """
-        
-        df_exclusions_sEEG_ES = df.dropna(subset=[sEEG_ES], axis=0, inplace=False)
-        return df_exclusions_sEEG_ES
+def exclude_sEEG_ES(df):
+    """
+    exclude stereoEEG and electrical stimulation cases on the fly and
+    as the data grows, rather than using pickled resources
+    """
+    df_exclusions_sEEG_ES = df.dropna(subset=[sEEG_ES], axis=0, inplace=False)
+    return df_exclusions_sEEG_ES
