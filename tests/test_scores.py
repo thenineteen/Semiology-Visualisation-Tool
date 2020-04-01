9e4cf8ed14d3d7b5d377dd4600d7cd12e52c57d8
Fernando Perez-Garcia <fepegar@gmail.com>
2020-04-01 20:33:49 +0100

    Add test for semiology term that used to fail

--- tests/test_scores.py
+++ tests/test_scores.py
@@ -2,5 +2,10 @@ import unittest
 from mega_analysis import get_scores_dict
 
 class TestScores(unittest.TestCase):
-    def test_scores_dict(self):
-        scores_dict = get_scores_dict(catch_errors=False)
+    def test_existing_aphasia(self):
+        scores_dict = get_scores_dict('Aphasia', catch_errors=False)
+        self.assertIs(type(scores_dict), dict)
+
+    def test_existing_aphemia(self):
+        scores_dict = get_scores_dict('Aphemia', catch_errors=False)
+        self.assertIs(type(scores_dict), dict)
