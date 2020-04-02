34809986bb41ceb883747af3bfdc9b43b20dbe85
The Nineteen <thenineteen@gmail.com>
2020-04-02 14:30:32 +0100

    return none if no data

--- mega_analysis/crosstab/mega_analysis/QUERY_SEMIOLOGY.py
+++ mega_analysis/crosstab/mega_analysis/QUERY_SEMIOLOGY.py
@@ -227,6 +227,9 @@ def QUERY_SEMIOLOGY(df, semiology_term=['love'],
         inspect_result['Localising'] = 0
     if 'Lateralising' not in inspect_result.columns:
         inspect_result['Lateralising'] = 0
+    if ((inspect_result['Localising'] == 0) & (inspect_result['Lateralising'] == 0)):
+        print('QUERY SEMIOLOGY ERROR: This semiology was not found within the reported literature nor in the semiology categories')
+        return None
 
     try:
         inspect_result.drop_duplicates(inplace=True)
