cc81dabb2c9ba1961fd9007565140d58cbd39d55
Fernando Perez-Garcia <fepegar@gmail.com>
2020-04-02 15:11:50 +0100

    Add Semiology class

--- mega_analysis/scores.py
+++ mega_analysis/scores.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from typing import Optional
+
 import yaml
 import numpy as np
 import pandas as pd
@@ -39,7 +41,6 @@ gif_lat_file = pd.read_excel(
     sheet_name='Full GIF Map for Review '
 )
 
-
 def recursive_items(dictionary):
     """https://stackoverflow.com/a/39234154/3956024"""
     for key, value in dictionary.items():
@@ -53,6 +54,81 @@ def get_all_semiology_terms():
     with open(semiology_dict_path) as f:
         dictionary = yaml.load(f, Loader=yaml.FullLoader)
     return sorted(recursive_items(dictionary))
+
+# Read YAML
+all_semiology_terms = get_all_semiology_terms()
+
+# Define constants
+LEFT = 'L'
+RIGHT = 'R'
+
+
+class Semiology:
+    def __init__(
+            self,
+            term: str,
+            left: bool = False,
+            right: bool = False,
+            neutral: bool = False,
+            seizure_freedom: bool = True,
+            concordance: bool = True,
+            seeg_es: bool = True,
+            et_topology_ez: bool = True,
+            ):
+        self.term = term
+        self.left = left
+        self.right = right
+        self.neutral = neutral
+        self.seizure_freedom = seizure_freedom
+        self.concordance = concordance
+        self.seeg_es = seeg_es
+        self.et_topology_ez = et_topology_ez
+
+    def query_semiology(self):
+        if self.term in all_semiology_terms:
+            path = semiology_dict_path
+        else:
+            path = None
+        inspect_result = QUERY_SEMIOLOGY(
+            df,
+            semiology_term=self.term,
+            semiology_dict_path=path,
+        )
+        return inspect_result
+
+    def get_symptoms_side(self):
+        if self.left:
+            return LEFT
+        elif self.right:
+            return RIGHT
+        else:
+            raise ValueError('Choose left or right symptoms side')
+
+    def get_dominant_hemisphere(self):
+        return LEFT  # TODO: check with Ali & Gloria
+
+    def query_lateralisation(self):
+        query_semiology_result = self.query_semiology()
+        all_combined_gifs = QUERY_LATERALISATION(
+            query_semiology_result,
+            df,
+            map_df_dict,
+            gif_lat_file,
+            side_of_symptoms_signs=self.get_symptoms_side(),
+            pts_dominant_hemisphere_R_or_L=self.get_dominant_hemisphere(),
+        )
+        return all_combined_gifs
+
+    def get_num_patients_dict(self):
+        query_lateralisation_result = self.query_lateralisation()
+        array = np.array(query_lateralisation_result)
+        _, labels, patients = array.T
+        num_patients_dict = {
+            int(label): float(num_patients)
+            for (label, num_patients)
+            in zip(labels, patients)
+        }
+        return num_patients_dict
 
 
 def get_scores(
@@ -142,5 +218,7 @@ def get_scores_dict(
     return scores_dict
 
 
+
 if __name__ == "__main__":
-    get_scores_dict()
+    semiology = Semiology('Aphasia', left=True)
+    semiology.get_num_patients_dict()
--- tests/test_scores.py
+++ tests/test_scores.py
@@ -1,11 +1,13 @@
 import unittest
-from mega_analysis import get_scores_dict
+from mega_analysis.scores import Semiology
 
 class TestScores(unittest.TestCase):
     def test_existing_aphasia(self):
-        scores_dict = get_scores_dict('Aphasia', catch_errors=False)
-        self.assertIs(type(scores_dict), dict)
+        semiology = Semiology('Aphasia', left=True)
+        num_patients_dict = semiology.get_num_patients_dict()
+        self.assertIs(type(num_patients_dict), dict)
 
     def test_existing_aphemia(self):
-        scores_dict = get_scores_dict('Aphemia', catch_errors=False)
-        self.assertIs(type(scores_dict), dict)
+        semiology = Semiology('Aphemia', left=True)
+        num_patients_dict = semiology.get_num_patients_dict()
+        self.assertIs(type(num_patients_dict), dict)
