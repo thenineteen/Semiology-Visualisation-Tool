afcda5eb0e640caeae3cb1eb9233138304df22a9
The Nineteen <thenineteen@gmail.com>
2020-05-20 00:30:50 +0100

    factored file paths, function for lobes_mapping

---
+++ resources/gif_sheet_names.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def gif_sheet_names():
+    """
+    Update this if the sheet names change or more are added.
+    Used by semiology.py and gif_lobed_from_excel_sheets.py
+    """
+
+    gif_sheet_names=[
+        'GIF TL', 'GIF FL', 'GIF PL', 'GIF OL',
+        'GIF CING', 'GIF INSULA',
+        'GIF HYPOTHALAMUS', 'GIF CEREBELLUM',
+        ]
+
+    return gif_sheet_names
