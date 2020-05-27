2d96b7cc3c82d3702320c47d6ef9d45d4e87a024
The Nineteen <thenineteen@gmail.com>
2020-05-27 14:07:17 +0100

    update dummy_semiology_dict path

--- resources/file_paths.py
+++ resources/file_paths.py
@@ -8,8 +8,9 @@ def file_paths(dummy_data=False):
     test_dir = repo_dir / 'tests'
     if dummy_data:
         excel_path = test_dir / '_dummy_data.xlsx'
+        semiology_dict_path = test_dir / '_dummy_semiology_dictionary.yaml'
     else:
         excel_path = resources_dir / 'syst_review_single_table.xlsx'
-    semiology_dict_path = resources_dir / 'semiology_dictionary.yaml'
+        semiology_dict_path = resources_dir / 'semiology_dictionary.yaml'
 
     return repo_dir, resources_dir, excel_path, semiology_dict_path
