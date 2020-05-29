fa820a0fe71061aac8d89a984a5542456229373d
The Nineteen <thenineteen@gmail.com>
2020-05-29 02:34:19 +0100

    more pep8 and removing import *

--- mega_analysis/crosstab/mega_analysis/QUERY_INTERSECTION_TERMS.py
+++ mega_analysis/crosstab/mega_analysis/QUERY_INTERSECTION_TERMS.py
@@ -1,6 +1,7 @@
-from .QUERY_SEMIOLOGY import *
+from .QUERY_SEMIOLOGY import QUERY_SEMIOLOGY
 import numpy as np
 import pandas as pd
+
 
 def QUERY_INTERSECTION_TERMS(df, *args):
     """
@@ -18,22 +19,24 @@ def QUERY_INTERSECTION_TERMS(df, *args):
     for arg in args:
         n += 1
         query_inspection = QUERY_SEMIOLOGY(df, semiology_term=arg,
-                                            ignore_case=True, semiology_dict_path=None,
-                                            # col1=col1, col2=col1
-                                            )
+                                           ignore_case=True, semiology_dict_path=None,
+                                           # col1=col1, col2=col1
+                                           )
 
         # can't merge the first df
-        if n==1:
+        if n == 1:
             inspect_combined_result = query_inspection
             continue
 
         # ans is df of the intersection of the above two, keep the index otherwise it is reset
-        inspect_combined_result = query_inspection.reset_index().merge(inspect_combined_result, how="inner").set_index('index')
+        inspect_combined_result = query_inspection.reset_index().merge(
+            inspect_combined_result, how="inner").set_index('index')
         if inspect_combined_result.empty:
             print('No results combining ALL of those keyword terms, breaking. Try reducing number of terms or respelling.',
-                 'Note no semiology dictionary is used.')
+                  'Note no semiology dictionary is used.')
             break
 
     # clean up all nan columns
-    inspect_combined_result = inspect_combined_result.dropna(axis='columns', how='all')
+    inspect_combined_result = inspect_combined_result.dropna(
+        axis='columns', how='all')
     return inspect_combined_result
