2547587efa5ebe86979426577fca9dd243038e2c
The Nineteen <thenineteen@gmail.com>
2020-06-25 18:40:03 +0100

    test_localisation_compatibility

--- tests/test_get_lobes.py
+++ tests/test_get_lobes.py
@@ -2,11 +2,23 @@ import unittest
 
 from mega_analysis.crosstab.gif_lobes_from_excel_sheets import \
     gif_lobes_from_excel_sheets
+from mega_analysis.crosstab.semiology_all_localisations import \
+    all_localisations
+from mega_analysis.crosstab.semiology_lateralisation_localisation import \
+    semiology_lateralisation_localisation
 
 
-class TestQuerySemiology(unittest.TestCase):
-    def test_is_dict(self):
+class LocalisationLobesAndHierarchies(unittest.TestCase):
+    def test_lobes_is_dict(self):
         self.assertIsInstance(gif_lobes_from_excel_sheets(), dict)
 
-# test = TestQuerySemiology()
-# test.test_is_dict()
+    def test_localisation_compatibility(self):
+        """
+        Test the melted df_localisation has localisation terms which are ALL found in list all_localisations()
+        """
+        all_localisations_list_filtered = all_localisations()
+        df_localisation_melt = semiology_lateralisation_localisation()
+
+        mask = df_localisation_melt['Localisation'].isin(
+            all_localisations_list_filtered)
+        assert mask.all() == True
