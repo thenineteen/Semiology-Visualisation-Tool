6a3153e9ac7913defecbefd667e9b0a775cabd71
The Nineteen <thenineteen@gmail.com>
2020-08-06 01:05:44 +0100

    add beta version compatibility for loading

--- mega_analysis/crosstab/file_paths.py
+++ mega_analysis/crosstab/file_paths.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pathlib import Path
 
 
-def file_paths(dummy_data=False):
+def file_paths(dummy_data=False, **kwargs):
     # Define paths
     repo_dir = Path(__file__).parent.parent.parent
     resources_dir = repo_dir / 'resources'
@@ -10,6 +10,9 @@ def file_paths(dummy_data=False):
     if dummy_data:
         excel_path = test_dir / '_dummy_data.xlsx'
         semiology_dict_path = test_dir / '_dummy_semiology_dictionary.yaml'
+    elif "Beta" in kwargs:
+        excel_path = test_dir / 'syst_review_single_table.xlsx'
+        semiology_dict_path = test_dir / 'semiology_dictionary.yaml'
     else:
         excel_path = resources_dir / 'Semio2Brain Database.xlsx'
         semiology_dict_path = resources_dir / 'semiology_dictionary.yaml'
