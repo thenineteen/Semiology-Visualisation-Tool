135c99220c68573b2ed26542f19f3d6c40835f96
The Nineteen <thenineteen@gmail.com>
2020-08-12 21:47:19 +0100

    docstring typo

--- mega_analysis/crosstab/gif_sheet_names.py
+++ mega_analysis/crosstab/gif_sheet_names.py
@@ -4,7 +4,7 @@ import pandas as pd
 def gif_sheet_names():
     """
     Update this if the sheet names change or more are added.
-    Used by semiology.py and gif_lobed_from_excel_sheets.py
+    Used by semiology.py and gif_lobes_from_excel_sheets.py
     """
 
     gif_sheet_names = [
