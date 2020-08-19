e11837d9845d70c884a9c5ff7a500c2577f2d7c4
The Nineteen <thenineteen@gmail.com>
2020-08-19 23:16:55 +0100

    debug

--- debugging_multi_terms_neutral_only.py
+++ debugging_multi_terms_neutral_only.py
@@ -8,6 +8,10 @@ from mega_analysis.crosstab.mega_analysis.MEGA_ANALYSIS import MEGA_ANALYSIS
 file = Path(__file__).parent/'resources'/'semiologies_neutral_only.txt'
 list_of_terms = list(open(file, 'r'))
 
+# list_of_terms = ['Mimetic Automatisms',
+#                  'No Semiology - Only Stimulation Studies',
+#                  'Non-Specific Aura', 'Olfactory-Gustatory', 'Palilalia', 'Psychic', 'Spitting', 'Vestibular', 'Vocalisation', 'Whistling']
+
 for term in list_of_terms:
     patient = Semiology(
         # 'Figure of 4',
