0d9e3302c5395ff08c98155d1f4eba91d520dbab
The Nineteen <thenineteen@gmail.com>
2020-08-19 13:04:48 +0100

    Automatisms Combination - Manual LowerLimb Oral added to SemioDict

--- debugging_sadeq.py
+++ debugging_sadeq.py
@@ -1,48 +1,56 @@
 import mega_analysis
 from mega_analysis import Semiology, Laterality
+from pathlib import Path
 
 from mega_analysis.crosstab.file_paths import file_paths
 from mega_analysis.crosstab.mega_analysis.MEGA_ANALYSIS import MEGA_ANALYSIS
 
-patient = Semiology(
-    # 'Figure of 4',
-    # symptoms_side=Laterality.LEFT,
-    # dominant_hemisphere=Laterality.LEFT,
+file = Path(__file__).parent/'resources'/'semiologies_neutral_also.txt'
+list_of_terms = list(open(file, 'r'))
+for term in list_of_terms:
 
-    # 'Blink',
-    # Laterality.NEUTRAL,
-    # Laterality.LEFT,
+    patient = Semiology(
+        # 'Figure of 4',
+        # symptoms_side=Laterality.LEFT,
+        # dominant_hemisphere=Laterality.LEFT,
 
-    # 'All Automatisms (oral, automotor)',
-    # Laterality.LEFT,
-    # Laterality.LEFT,
+        # 'Blink',
+        # Laterality.NEUTRAL,
+        # Laterality.LEFT,
 
-    # 'Grimace', Laterality.NEUTRAL, Laterality.NEUTRAL,
+        # 'All Automatisms (oral, automotor)',
+        # Laterality.LEFT,
+        # Laterality.LEFT,
 
-    'latexceedsloc',
-    symptoms_side=Laterality.LEFT,
-    dominant_hemisphere=Laterality.LEFT,
-)
+        # 'Grimace', Laterality.NEUTRAL, Laterality.NEUTRAL,
 
+        # 'latexceedsloc',
+        # symptoms_side=Laterality.LEFT,
+        # dominant_hemisphere=Laterality.LEFT,
 
-###
-##
-# if we want to use the dummy_data instead of real Semio2Brain DataFrame:
-repo_dir, resources_dir, dummy_data_path, dummy_semiology_dict_path = \
-    file_paths(dummy_data=True)
+        term.strip(),
+        symptoms_side=Laterality.LEFT,
+        dominant_hemisphere=Laterality.LEFT,
+    )
 
-patient.data_frame, _, _ = MEGA_ANALYSIS(
-    excel_data=dummy_data_path,
-    n_rows=100,
-    usecols="A:DH",
-    header=1,
-    exclude_data=False,
-    plot=True,
-)
-#
-##
-###
+    ###
+    ##
+    # # if we want to use the dummy_data instead of real Semio2Brain DataFrame:
+    # repo_dir, resources_dir, dummy_data_path, dummy_semiology_dict_path = \
+    #     file_paths(dummy_data=True)
 
+    # patient.data_frame, _, _ = MEGA_ANALYSIS(
+    #     excel_data=dummy_data_path,
+    #     n_rows=100,
+    #     usecols="A:DH",
+    #     header=1,
+    #     exclude_data=False,
+    #     plot=True,
+    # )
+    #
+    ##
+    ###
 
-heatmap = patient.get_num_datapoints_dict()
-print('Result:', heatmap)
+    heatmap = patient.get_num_datapoints_dict()
+    print("\nSemiology: ", term)
+    print('\nResult:', heatmap, '\n')
