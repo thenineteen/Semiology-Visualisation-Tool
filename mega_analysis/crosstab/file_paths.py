df491a027447dca5c49787c6e0722473f922f806
The Nineteen <thenineteen@gmail.com>
2020-09-08 00:27:23 +0100

    move dummy_data mappings to tests/mappings #17

--- mega_analysis/crosstab/file_paths.py
+++ mega_analysis/crosstab/file_paths.py
@@ -7,16 +7,18 @@ def file_paths(dummy_data=False, **kwargs):
     repo_dir = Path(__file__).parent.parent.parent
     test_dir = repo_dir / 'tests'
     resources_dir = repo_dir / 'resources'
-    mappings_folder = resources_dir / 'mappings'
 
     if dummy_data:
         Database_path = test_dir / '_dummy_data.csv'
         SemioDict_path = test_dir / '_dummy_semiology_dictionary.yaml'
+        mappings_folder = test_dir / 'mappings'
     elif "Beta" in kwargs:
         Database_path = resources_dir / 'syst_review_single_table.xlsx'
         SemioDict_path = resources_dir / 'semiology_dictionary.yaml'
+        mappings_folder = resources_dir / 'mappings'
     else:
         Database_path = resources_dir / 'Semio2Brain Database.xlsx'
         SemioDict_path = resources_dir / 'semiology_dictionary.yaml'
+        mappings_folder = resources_dir / 'mappings'
 
     return repo_dir, resources_dir, Database_path, SemioDict_path, mappings_folder
