b1bf0c3a96304e84255d24a2161c58e415fccbee
Karan Dahele <karandahele@gmail.com>
2021-01-24 16:49:57 +0000

    delete axis requirement - already in conf int func

--- scripts/figures/bootstrapping.py
+++ scripts/figures/bootstrapping.py
@@ -27,25 +27,18 @@ def bootstrap_frequency_array(counts, n_samples=1000, alpha = 0.05):
         
     return np.array([lower_limits, upper_limits])
 
-def bootstrap_frequency_matrix(counts_df, n_samples=1000, axis='semiology'):
+def bootstrap_frequency_matrix(counts_df, n_samples=1000, alpha=0.05):
     """
         Calculate bootstrapped confidence intervals for 2 dimensional matrix of 
         categorical frequencies. 2d wrapper for 1d function bootstrap_frequency_array
 
         (usually pass in counts_df)
     """
-    if axis == 'zone':
-        counts_df = counts_df.T
-    elif axis == 'semiology':
-        pass
-    else:
-        raise ValueError('axis must be given from {semiology, zone}')
-
     upper_ci_counts = pd.DataFrame(index = counts_df.index, columns = counts_df.columns)
     lower_ci_counts = pd.DataFrame(index = counts_df.index, columns = counts_df.columns)
 
     for column in counts_df.columns:
-        lower_limits, upper_limits = bootstrap_frequency_array(counts_df[column].values, n_samples)
+        lower_limits, upper_limits = bootstrap_frequency_array(counts_df[column].values, n_samples, alpha)
         lower_ci_counts[column] = lower_limits
         upper_ci_counts[column] = upper_limits
         
