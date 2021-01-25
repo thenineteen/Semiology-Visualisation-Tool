fixed normalisation other splitTL

mega_analysis/Sankey_Functions.py
@@ -126,7 +126,7 @@ def normalise_localisation_cols_OTHER_SplitTL(df, **kwargs):
 
         # now go back to do top level normalisation
         df_temp.loc[:, 'ratio'] = df_temp['Localising'] / \
-            (df_temp[LobesOTHER].sum(axis=1))
+            (df_temp[LobesOTHER_splitTL].sum(axis=1))
         df_temp = df_temp.astype({'ratio': 'float'})
         df_temp.loc[:, LobesOTHER_splitTL] = (df_temp.loc[:, LobesOTHER_splitTL]).multiply(
             df_temp.loc[:, 'ratio'], axis=0)